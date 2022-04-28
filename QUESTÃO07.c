0da8487a59add2e7b0dccf853c65bd5c261cb193
gui1342 <103466943+gui1342@users.noreply.github.com>
2022-04-28 15:03:54 -0300

    Update QUESTÃO07.c

--- QUESTÃO07.c
+++ QUESTÃO07.c
@@ -1,4 +1,6 @@
-//7. Para que a divisão entre 2 números possa ser realizada, o divisor não pode ser nulo (zero). Escreva um programa para ler 2 valores e imprimir o resultado da divisão do primeiro pelo segundo. OBS: O programa deve validar a leitura do segundo valor (que não deve ser nulo). Enquanto for fornecido um valor nulo a leitura deve ser repetida.
+/*7. Para que a divisão entre 2 números possa ser realizada, o divisor não pode ser nulo (zero). Escreva um programa para ler 2 valores e imprimir o resultado da 
+divisão do primeiro pelo segundo. OBS: O programa deve validar a leitura do segundo valor (que não deve ser nulo). Enquanto for fornecido um valor nulo a leitura 
+deve ser repetida.*/
 
 #include <stdio.h>
 
