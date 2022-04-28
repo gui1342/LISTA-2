b612e26d18b24b601c746ae215cf4d5820174cac
gui1342 <103466943+gui1342@users.noreply.github.com>
2022-04-28 14:48:25 -0300

    Update QUESTÃO06.c

--- QUESTÃO06.c
+++ QUESTÃO06.c
@@ -1,4 +1,7 @@
-//6. Escreva um programa que transforme o computador numa urna eletrônica para eleição para presidente de um certo país, às quais concorrem os candidatos 5-Paulo e 7-Renata. Cada voto deve ser dado pelo número do candidato, permitindo-se ainda o voto 0 para voto em branco. Qualquer voto diferente dos já citados é considerado nulo; em qualquer situação, o eleitor deve ser consultado quanto à confirmação do seu voto. No final da eleição o programa deve emitir um relatório contendo as porcentagens de votação de cada candidato, votos em branco, votos nulos e o candidato eleito. Obs.: O código para finalizar a urna (votação) é o usuário digitar algum número negativo.
+/*6. Escreva um programa que transforme o computador numa urna eletrônica para eleição para presidente de um certo país, às quais concorrem os candidatos 5-Paulo e 7-Renata. 
+Cada voto deve ser dado pelo número do candidato, permitindo-se ainda o voto 0 para voto em branco. Qualquer voto diferente dos já citados é considerado nulo; em qualquer 
+situação, o eleitor deve ser consultado quanto à confirmação do seu voto. No final da eleição o programa deve emitir um relatório contendo as porcentagens de votação de 
+cada candidato, votos em branco, votos nulos e o candidato eleito. Obs.: O código para finalizar a urna (votação) é o usuário digitar algum número negativo.*/
 
 #include <stdio.h>
 
