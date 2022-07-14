2827913c6dee8643cf7f3960ca7b06a0b2449000
gui1342 <103466943+gui1342@users.noreply.github.com>
2022-07-14 16:20:14 -0300

    Update QUESTÃO14.c

--- QUESTÃO14.c
+++ QUESTÃO14.c
@@ -1,4 +1,5 @@
-//14. Elabore um programa que apresente a quantidade de números divisíveis por 3 pertencentes a faixa A,B especificada pelo usuário. O programa deve funcionar tanto para A > B quanto para B > A.
+/*14. Elabore um programa que apresente a quantidade de números divisíveis por 3 pertencentes a 
+faixa A,B especificada pelo usuário. O programa deve funcionar tanto para A > B quanto para B > A.*/
 
 #include <stdio.h>
 #include <stdlib.h>
